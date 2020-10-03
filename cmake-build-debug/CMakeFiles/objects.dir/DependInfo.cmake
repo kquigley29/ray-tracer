689d4aab2ba8bd09ac2150a806180e0e0375815e
keane <keaneq@protonmail.com>
2020-10-03 13:14:09 +0100

    fill cmakelists.txt

---
+++ cmake-build-debug/CMakeFiles/objects.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/keane/Projects/ray-tracer/src/objects/Disc.cpp" "/home/keane/Projects/ray-tracer/cmake-build-debug/CMakeFiles/objects.dir/src/objects/Disc.cpp.o"
+  "/home/keane/Projects/ray-tracer/src/objects/Object.cpp" "/home/keane/Projects/ray-tracer/cmake-build-debug/CMakeFiles/objects.dir/src/objects/Object.cpp.o"
+  "/home/keane/Projects/ray-tracer/src/objects/Plane.cpp" "/home/keane/Projects/ray-tracer/cmake-build-debug/CMakeFiles/objects.dir/src/objects/Plane.cpp.o"
+  "/home/keane/Projects/ray-tracer/src/objects/Sphere.cpp" "/home/keane/Projects/ray-tracer/cmake-build-debug/CMakeFiles/objects.dir/src/objects/Sphere.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/raytracer.cpp
@@ -0,0 +1,3 @@
+//
+// Created by keane on 03/10/2020.
+//
