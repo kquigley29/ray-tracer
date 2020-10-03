369bac58d1acff3ec99f1c37ca2c7fcceb89c733
Angus Stanton <angusstanton225@gmail.com>
2020-10-03 14:23:16 +0100

    Added ray struct dets

--- include/raytracer/Ray.h
+++ include/raytracer/Ray.h
@@ -1,8 +1,20 @@
 //
 // Created by angus on 03/10/2020.
 //
+#include<eigen3/Eigen/Eigen>
 
 #ifndef RAYTRACER_RAY_H
 #define RAYTRACER_RAY_H
 
 #endif //RAYTRACER_RAY_H
+
+using Eigen::Vector3d;
+
+struct Ray{
+    Vector3d origin;
+    Vector3d direction;
+    Ray(Vector3d origin, Vector3d direction){
+        this->origin = origin;
+        this->direction = direction;
+    }
+};
