6902bdd7382256cb9d1052d4ab17ba5d40d6c864
keane <keaneq@protonmail.com>
2020-10-04 21:16:34 +0100

    add disc to test scene

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -35,9 +35,20 @@ int main(int argc, char** argv){
 
     Camera camera(Vector3d(0,0,3), Vector3d(0,1,-0.5), 0.7, res);
 
-    std::vector<Light*> lights = {new PointLight(Vector3d(0,-1,4), Vector3d(3000,1000,1000)),new PointLight(Vector3d(0,5,1), Vector3d(500,500,500)), new PointLight(Vector3d(0,5,7), Vector3d(2000,2000,3000))};
-    std::vector<Object*> objects = {new Plane(Vector3d(5,5,0), Vector3d(-1,-1,0), Material(0.5,1,0.5)), new Plane(Vector3d(0,0,-2), Vector3d(0,0,1), Material(1,1,1)),new Plane(Vector3d(-3,0,0), Vector3d(1,0,0), Material(1,1,1)), new Plane(Vector3d(0,12,0), Vector3d(0,-1,0), Material(1,1,1)),new Sphere(Vector3d(2.5,5,0), 1, Material(1,1,1)), new Sphere(Vector3d(-2.5,5,0), 1, Material(1,0.5,1)), new Sphere(Vector3d(0,3,0), 1, Material(1,0.5,1)), new Sphere(Vector3d(0,7,0), 1, Material(1,1,0.7))};
-//    std::vector<Object*> objects = {(new Disc(Vector3d(0,5,0), Vector3d(-1,-1,0), 2, Material(0.5,1,0.5)))};
+    std::vector<Light*> lights = {new PointLight(Vector3d(0,-1,4),
+                                  Vector3d(3000,1000,1000)),
+                                  new PointLight(Vector3d(0,5,1),Vector3d(500,500,500)),
+                                  new PointLight(Vector3d(0,5,7), Vector3d(2000,2000,3000))};
+
+    std::vector<Object*> objects = {new Plane(Vector3d(5,5,0),Vector3d(-1,-1,0), Material(0.5,1,0.5)),
+                                    new Plane(Vector3d(0,0,-2), Vector3d(0,0,1), Material(1,1,1)),
+                                    new Plane(Vector3d(-3,0,0), Vector3d(1,0,0), Material(1,1,1)),
+                                    new Plane(Vector3d(0,12,0), Vector3d(0,-1,0), Material(1,1,1)),
+                                    new Sphere(Vector3d(2.5,5,0), 1, Material(1,1,1)),
+                                    new Sphere(Vector3d(-2.5,5,0), 1, Material(1,0.5,1)),
+                                    new Sphere(Vector3d(0,3,0), 1, Material(1,0.5,1)),
+                                    new Sphere(Vector3d(0,7,0), 1, Material(1,1,0.7)),
+                                    new Disc(Vector3d(0,5,0), Vector3d(-1,-1,0), 2, Material(0.5,1,0.5))};
     Scene scene(camera, lights, objects);
 
   //  cv::Mat image = scene.render();
