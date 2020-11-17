ee472e73a8288848e18a1463284a8fb19f4e657a
keane <keaneq@protonmail.com>
2020-11-17 13:14:19 +0000

    use switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,38 @@
 #include "raytracer/raytracer.h"
 
 
-int main(int argc, char** argv){
-    Scene scene;
-    if (argc == 2)
-    {
-        scene.generate_scene(argv[1]);
-        cv::Mat image = scene.render_multithreaded();
-        cv::imshow("image", image);
-        cv::waitKey();
-        return 0;
-    }
+void print_help()
+{
+    printf("[Usage]\n"
+                "./trace <cfg file>\n"
+                "./trace <cfg file> <multi / solo>\n");
+}
 
-    if (argc == 3)
+
+int main(int argc, char** argv)
+{
+    Scene scene;
+    cv::namedWindow("image");
+    cv::Mat image;
+    switch (argc)
     {
-        scene.generate_scene(argv[1]);
-        cv::Mat image;
-        if (strcmp(argv[2], "multi") == 0) image = scene.render_multithreaded();
-        else if (strcmp(argv[2], "solo") == 0) image = scene.render();
-        cv::imshow("image", image);
-        cv::waitKey();
-        return 0;
+        case 2:
+            scene.generate_scene(argv[1]);
+            image = scene.render_multithreaded();
+            cv::imshow("image", image);
+            cv::waitKey();
+            return 0;
+
+        case 3:
+            scene.generate_scene(argv[1]);
+            if (strcmp(argv[2], "multi") == 0) image = scene.render_multithreaded();
+            else if (strcmp(argv[2], "solo") == 0) image = scene.render();
+            cv::imshow("image", image);
+            cv::waitKey();
+            return 0;
+
+        default:
+            print_help();
+            return 1;
     }
-    return 1;
 }
